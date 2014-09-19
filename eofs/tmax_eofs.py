be6d25ec13b37dfbda21c8c828a243e8972f9226
tammasfl <tammasfl@bf10338f-ac7c-4b5f-ab65-3350bd5bcc87>
2014-09-19 02:15:40 +0000

    Initial commit. This adds the eofs directory to the heatwaves project and puts some example eof scripts in it.

---
+++ eofs/tmax_eofs.py
@@ -0,0 +1,45 @@
+"""
+Calculate the eofs of tmax from AWAP data.
+This is a crappy test script.
+"""
+from netCDF4 import Dataset
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+import numpy as np
+from eofs.standard import Eof
+
+# Load the data from file.
+filename = 'AWAP_tmax_1951-2009_1deg.nc'
+ncin = Dataset(filename, 'r')
+temp = ncin.variables['tmax'][:]
+# No need to remove the mean from the data. Apparently the EOF solver does this automatically.
+#temp_mean = temp.mean(axis=0)
+#temp_anom = temp - temp_mean
+lon = ncin.variables['lon'][:]
+lat = ncin.variables['lat'][:]
+time = ncin.variables['time'][:]
+
+#Set up the EOF solver and find the first eof and pc.
+solver = Eof(temp)
+eof1 = solver.eofsAsCovariance(neofs=1)
+pc1 = solver.pcs(npcs=1)
+
+# Plotting.
+# Use an equidistant cylyndrical map projection.
+m = Basemap(projection='cyl', llcrnrlat=-44, urcrnrlat=-10, llcrnrlon=112, urcrnrlon=156)
+x, y = m(*np.meshgrid(lon, lat))
+#levs = np.arange(-8.,8.,1.)
+m.contourf(x, y, eof1.squeeze(), cmap=plt.cm.RdBu_r)
+parallels = np.arange(-45., -10., 10.)
+m.drawparallels(parallels, labels=[True,False,False,False])
+meridians = np.arange(115., 156., 10.,)
+m.drawmeridians(meridians, labels=[False,False,False,True])
+m.drawcoastlines()
+plt.title("EOF 1")
+cb = plt.colorbar(orientation='horizontal')
+#plt.show()
+plt.savefig('eof1.eps', format='eps')
+
+plt.figure()
+plt.plot(time, pc1)
+plt.savefig('pc1.eps', format='eps')
