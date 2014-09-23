Fixed the normalisation function and added a denomailse function to rotate.py.

eofs/tmax_eofs.py
@@ -12,14 +12,16 @@ from eofs.standard import Eof
 filename = 'AWAP_tmax_1951-2009_1deg.nc'
 ncin = Dataset(filename, 'r')
 temp = ncin.variables['tmax'][:]
-# No need to remove the mean from the data. Apparently the EOF solver does this automatically.
+# No need to remove the mean from the data. 
+# Apparently the EOF solver does this automatically.
 #temp_mean = temp.mean(axis=0)
 #temp_anom = temp - temp_mean
 lon = ncin.variables['lon'][:]
 lat = ncin.variables['lat'][:]
 nctime = ncin.variables['time'][:]
-# The time axis in the nc file is all screwy so I'll just use a fake time axis for now.
-time = np.arange(nctime.size) 
+# The time axis in the nc file is all screwy so 
+# I'll just use a fake time axis for now.
+time = np.arange(nctime.size)
 
 # Set up the EOF solver.
 solver = Eof(temp)
@@ -30,7 +32,9 @@ pc1 = solver.pcs(npcs=1, pcscaling=1)
 
 # Plotting.
 # Use an equidistant cylyndrical map projection.
-m = Basemap(projection='cyl', llcrnrlat=-44, urcrnrlat=-10, llcrnrlon=112, urcrnrlon=156)
+m = Basemap(projection='cyl', 
+            llcrnrlat=-44, urcrnrlat=-10, 
+            llcrnrlon=112, urcrnrlon=156)
 x, y = m(*np.meshgrid(lon, lat))
 #levs = np.arange(-8.,8.,1.)
 m.contourf(x, y, eof1.squeeze(), cmap=plt.cm.RdBu_r)