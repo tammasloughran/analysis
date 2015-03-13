61f11c72fb1a98c874a9a65270dd7a90847ffbae
tammasfl <tammasfl@bf10338f-ac7c-4b5f-ab65-3350bd5bcc87>
2015-03-13 00:53:05 +0000

    Masked values are zeroed before CCA

--- cca/ehf_pr_cca.py
+++ cca/ehf_pr_cca.py
@@ -4,6 +4,30 @@ import matplotlib.pyplot as plt
 from numpy import arange, meshgrid
 from mpl_toolkits.basemap import Basemap
 import load_data
+import numpy
+
+def timevsspace(data):
+    ntimes = data.shape[0]
+    npoints = data.shape[1]*data.shape[2]
+    reshaped_data = data.reshape([ntimes,npoints])
+    return reshaped_data
+
+def removenans(data):
+    index = numpy.where(numpy.isnan(data) == False)[0]
+    no_nan_data = data[index]
+    return no_nan_data, index
+
+def restorenans(data, shape, index):
+    with_nans = numpy.ones(shape)
+    with_nans[index] = data
+    return with_nans
+
+def mask_to_zero(data):
+    fill = numpy.zeros(data.shape)
+    index = numpy.where(data.mask == False)
+    fill[index] = data[index]
+    return fill
+
 prfile = ('/home/nfs/z5032520/heatwaves-svn/dataproc/summer_mslp_1911-2013.nc')
 hwfile = ('/media/Jupiter/reanalysis/AWAP/yearly/ehfhw/CCRC_NARCliM_1911-2014_EHFheatwaves_summer_AWAP0.5deg_detrended.nc')
 maskfile = ('/media/Jupiter/reanalysis/AWAP/mask/varmask.nc')
@@ -11,6 +35,7 @@ maskfile = ('/media/Jupiter/reanalysis/AWAP/mask/varmask.nc')
 #hwnc = Dataset(hwfile,'r')
 hwf, hwn, hwd, hwa, hwm, hwt, hw_lats, hw_lons, times\
         = load_data.load_heat_waves(hwfile)
+hwf = mask_to_zero(hwf)
 prnc = Dataset(prfile,'r')
 #mask = masknc.variables['mask'][:]
 pr = prnc.variables['mslp'][:]
@@ -25,16 +50,18 @@ R = CCA.rightPatterns()
 r = CCA.correlation()
 a = CCA.rightExpCoeffs()
 b = CCA.leftExpCoeffs()
-print (r)
+#print (r)
 map_axes = Basemap(projection='cyl',llcrnrlat=pr_lats[-1], urcrnrlat=pr_lats[0],llcrnrlon=pr_lons[0], urcrnrlon=pr_lons[-1])
 x, y = map_axes(*meshgrid(pr_lons, pr_lats))
 map_axes.contourf(x,y,L[:,:,0],cmap=plt.cm.RdBu_r)
 map_axes.drawcoastlines()
+cb = plt.colorbar()
 plt.show()
 map_axes = Basemap(projection='cyl',llcrnrlat=hw_lats[-1], urcrnrlat=hw_lats[0],llcrnrlon=hw_lons[0], urcrnrlon=hw_lons[-1])
 x, y = map_axes(*meshgrid(hw_lons, hw_lats))
 map_axes.contourf(x,y,R[:,:,0],cmap=plt.cm.RdBu_r)
 map_axes.drawcoastlines()
+cb = plt.colorbar()
 plt.show()
 plt.plot(a[:,0])
 plt.plot(b[:,0])
