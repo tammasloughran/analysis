a451037aecf80c695f881caf6f338ce1c1643e23
Tammas Loughran <t.loughran@student.unsw.edu.au>
2015-11-09 06:39:08 +0000

    loops

--- cca/hw_pr_cca.py
+++ cca/hw_pr_cca.py
@@ -19,12 +19,34 @@ pr_lons = prnc.variables['lon'][:]
 years = range(1911,2013)
 
 # Detrend and standardize.
+def standardize(hwmet):
+    """Standardize a heatwave metric by subtracting the long term mean and 
+    dividing by the long term standard deviation.
+
+    Arguments
+    hwmet -- heatwave metric.
+
+    Returns
+    hwmet -- heatwave metric standardized along the first dimension.
+    """
+    hwmet = hwmet - hwmet.mean(axis=0)
+    hwmet = hwmet/hwmet.std(axis=0)
+    return hwmet
 pr_detrended = detrend(pr, axis=0)
-pr_detrended = pr_detrended - pr_detrended.mean(axis=0)
-pr_detrended = pr_detrended/pr_detrended.std(axis=0)
+pr_detrended = standardize(pr_detrended)
 hwa_detrended = np.ma.array(detrend(hwa, axis=0), mask=hwa.mask)
-hwa_detrended = hwa_detrended - hwa_detrended.mean(axis=0)
-hwa_detrended = hwa_detrended/hwa_detrended.std(axis=0)
+hwa_detrended = standardize(hwa_detrended)
+hwm_detrended = np.ma.array(detrend(hwm, axis=0), mask=hwa.mask)
+hwm_detrended = standardize(hwm_detrended)
+hwf_detrended = np.ma.array(detrend(hwf, axis=0), mask=hwa.mask)
+hwf_detrended = standardize(hwf_detrended)
+hwd_detrended = np.ma.array(detrend(hwd, axis=0), mask=hwa.mask)
+hwd_detrended = standardize(hwd_detrended)
+hwn_detrended = np.ma.array(detrend(hwd, axis=0), mask=hwa.mask)
+hwn_detrended = standardize(hwd_detrended)
+hwt_detrended = np.ma.array(detrend(hwt, axis=0), mask=hwa.mask)
+hwt_detrended = standardize(hwt_detrended)
+
 
 # Calculate significant number of eofs using North test
 def nsigpcs(varfrac, north):
@@ -35,34 +57,37 @@ def nsigpcs(varfrac, north):
     clear = upper<lower
     return np.argmax(clear==False)
 
-pca = Eof(pr_detrended)
-pr_sig = nsigpcs(pca.varianceFraction(), pca.northTest(vfscaled=True))
-#import pdb
-#pdb.set_trace()
-pca = Eof(hwa_detrended)
-hw_sig = nsigpcs(pca.varianceFraction(), pca.northTest(vfscaled=True))
-print hw_sig, pr_sig
-# Perform CCA
-CCA = BPCCA(hwa_detrended[:-3,...], pr_detrended, (hw_sig,pr_sig))
-L = CCA.leftPatterns()
-R = CCA.rightPatterns()
-a = CCA.rightExpCoeffs()
-b = CCA.leftExpCoeffs()
-
 if __name__=='__main__':
-    lmask, rmask = CCA.MCTestMask(250,100,0.05)
-    # Print corelations
-    print CCA.correlation()
-    # Variance fraction
-    #print CCA.varianceFractions()
-    # Plot
-    pattern = 0
-    # Left paterns
-    cca_plot.plot_cp(L[...,pattern], hw_lons, hw_lats, 'HWA', 
-            lmask[...,pattern])
-    # Right paterns
-    cca_plot.plot_cp(R[...,pattern], pr_lons, pr_lats, 'Pressure', 
-            rmask[...,pattern])
-    # Plot coeficients
-    cca_plot.plot_coefs(a[:,pattern], b[:,pattern], years,
-            llabel='HWA', rlabel='Pressure')
+    pca = Eof(pr_detrended)
+    pr_sig = nsigpcs(pca.varianceFraction(), pca.northTest(vfscaled=True))
+    metric_dict = {"HWN":hwn,"HWF":hwf,"HWD":hwd,"HWA":hwa,"HWM":hwm,"HWT":hwt}
+    for metric_name in metric_dict.keys():
+        pca = Eof(metric_dict[metric_name])
+        hw_sig = nsigpcs(pca.varianceFraction(), pca.northTest(vfscaled=True))
+        if hw_sig<2: hw_sig = 2
+        # Perform CCA
+        CCA = BPCCA(metric_dict[metric_name][:-3,...], pr_detrended, (hw_sig,4))
+        L = CCA.leftPatterns()
+        R = CCA.rightPatterns()
+        a = CCA.rightExpCoeffs()
+        b = CCA.leftExpCoeffs()
+    
+        #lmask, rmask = CCA.MCTestMask(250,100,0.05)
+        # Print corelations
+        print metric_name
+        print CCA.correlation()
+        # Variance fraction
+        print np.array(CCA.varianceFractions())
+        # Plot
+        for pattern in range(2):
+            # Left paterns
+            cca_plot.plot_cp(L[...,pattern], hw_lons, hw_lats, 
+                    '%d %s'%(pattern+1,metric_name))
+            #        lmask[...,pattern])
+            # Right paterns
+            cca_plot.plot_cp(R[...,pattern], pr_lons, pr_lats,
+                    '%d Pressure %s'%(pattern+1,metric_name))
+            #        rmask[...,pattern])
+            # Plot coeficients
+            cca_plot.plot_coefs(a[:,pattern], b[:,pattern], years,
+                    llabel='%d %s'%(pattern+1,metric_name), rlabel='Pressure')
